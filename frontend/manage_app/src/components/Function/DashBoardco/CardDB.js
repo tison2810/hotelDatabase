0766b92995e48255b6593ffc8924b4f3c5af377e
Huy Dang <huy.dang110902@gmail.com>
2022-12-04 00:09:07 +0700

    1.3 updated

--- frontend/manage_app/src/components/Function/DashBoardco/CardDB.js
+++ frontend/manage_app/src/components/Function/DashBoardco/CardDB.js
@@ -5,10 +5,10 @@ import styles from "../../../css/CardDB.module.css"
 const CardDB = () => {
     return (
         <div className={styles.sales}>
-                    <div className={styles.logo}>
-                        <SiGoogleanalytics width={2} height={2} />
-                    </div>
-                    <div className={styles.middle}>
+                <div className={styles.logo}>
+                    <SiGoogleanalytics width={2} height={2} />
+                </div>
+                <div className={styles.middle}>
                         <div className={styles.left}>
                             <h3>Total Sales</h3>
                             <h1>$25,024</h1>
@@ -17,12 +17,13 @@ const CardDB = () => {
                             <svg>
                                 <circle cx='38' cy='38' r='36'></circle>
                             </svg>
-                            <div className={styles.number}>
-                                <p>81%</p>
-                    </div>
-                    <small>Last 24 Hours</small>
-                        </div>
-                    </div>
+                        </div> 
+                        <div className={styles.number}>
+                            <p>81%</p>
+                        </div>    
+                </div>
+               <small>Last 24 Hours</small>
+                       
         </div>
     )
 }
