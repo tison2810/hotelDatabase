1f36f9f542e2680ac2a92d4747162a44ab0f1086
Huy Dang <huy.dang110902@gmail.com>
2022-12-04 00:41:51 +0700

    1.5 updated

--- frontend/manage_app/src/components/Function/DashBoardco/CardDB.js
+++ frontend/manage_app/src/components/Function/DashBoardco/CardDB.js
@@ -9,19 +9,18 @@ const CardDB = () => {
                     <SiGoogleanalytics width={2} height={2} />
                 </div>
                 <div className={styles.middle}>
-                        <div className={styles.left}>
+                    <div className={styles.left}>
                             <h3>Total Sales</h3>
                             <h1>$25,024</h1>
-                        </div>
-                        <div className={styles.progress}>
+                    </div>
+                    <div className={styles.progress}>
                             <svg>
                                 <circle cx='38' cy='38' r='36'></circle>
                             </svg>
-                       
-                        <div className={styles.number}>
-                            <p>81%</p>
-                         </div>    
-                     </div> 
+                            <div className={styles.number}>
+                                <p>81%</p>
+                            </div>        
+                    </div> 
                 </div>
                <small>Last 24 Hours</small>
                        
