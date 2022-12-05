6b6ea8c2a017245c922375ac894bad910f035d19
danisolation <78058950+danisolation@users.noreply.github.com>
2022-12-05 21:26:59 +0700

    Add files via upload

--- database/cau3.sql
+++ database/cau3.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE getRevenue(
 	Ma_KH_n INT
 )
 BEGIN
-	SELECT khach_hang.Ma_KH, khach_hang.Ten, a.sum_gui + b.sum_nhan AS sum
+	SELECT khach_hang.Ma_KH, khach_hang.Ten, a.sum_gui + b.sum_nhan AS doanh_thu
         FROM ((khach_hang
         INNER JOIN (SELECT ma_kh_gui, SUM(Phi_noi_thanh) AS sum_gui FROM bien_ban_gui GROUP BY ma_kh_gui) a  ON a.ma_kh_gui = khach_hang.Ma_KH)
         INNER JOIN (SELECT ma_kh_nhan, SUM(Phi_noi_thanh + Phi_lien_tinh) AS sum_nhan FROM bien_ban_nhan GROUP BY ma_kh_nhan) b ON b.ma_kh_nhan = khach_hang.Ma_KH)
@@ -16,31 +16,31 @@ CREATE PROCEDURE getAdrress(
     Ngay_gui_n DATE
 )
 BEGIN
-	select b.Ma_KH as ma_kh_gui, b.ten_nguoi_gui, bien_ban_nhan.Ma_bien_ban, b.Ngay_gui,bien_ban_nhan.Ngay_nhan, b.Noi_den, khach_hang.Ten as kh_nhan
-	from ((bien_ban_nhan
-	inner join 
-		(select a.Ma_KH, a.ten_nguoi_gui, a.Ngay_gui, kien_hang.Noi_den,kien_hang.ma_bien_ban_gui, nhan_hang.Ma_bien_ban_nhan
-		from ((kien_hang
-		inner join nhan_hang on kien_hang.ID = nhan_hang.ID_kien_hang)
-		inner join 
-			(select khach_hang.Ma_KH as Ma_KH, bien_ban_gui.Ngay_gui ,khach_hang.Ten as ten_nguoi_gui, bien_ban_gui.Ma_bien_ban as ma_bien_ban_gui
-			from bien_ban_gui
-			inner join khach_hang on khach_hang.Ma_KH = bien_ban_gui.ma_kh_gui) a on a.ma_bien_ban_gui = kien_hang.ma_bien_ban_gui)) b 
-		on b.Ma_bien_ban_nhan = bien_ban_nhan.Ma_bien_ban)
-	inner join khach_hang on khach_hang.Ma_kh = bien_ban_nhan.ma_kh_nhan)
-	having b.Ma_KH = Ma_KH_n AND b.Ngay_gui = Ngay_gui_n;
+	SELECT b.Ma_KH AS ma_kh_gui, b.ten_nguoi_gui, bien_ban_nhan.Ma_bien_ban, b.Ngay_gui,bien_ban_nhan.Ngay_nhan, b.Noi_den, khach_hang.Ten AS kh_nhan
+	FROM ((bien_ban_nhan
+	INNER JOIN 
+		(SELECT a.Ma_KH, a.ten_nguoi_gui, a.Ngay_gui, kien_hang.Noi_den,kien_hang.ma_bien_ban_gui, nhan_hang.Ma_bien_ban_nhan
+		FROM ((kien_hang
+		INNER JOIN nhan_hang ON kien_hang.ID = nhan_hang.ID_kien_hang)
+		INNER JOIN 
+			(SELECT khach_hang.Ma_KH AS Ma_KH, bien_ban_gui.Ngay_gui ,khach_hang.Ten AS ten_nguoi_gui, bien_ban_gui.Ma_bien_ban AS ma_bien_ban_gui
+			FROM bien_ban_gui
+			INNER JOIN khach_hang ON khach_hang.Ma_KH = bien_ban_gui.ma_kh_gui) a ON a.ma_bien_ban_gui = kien_hang.ma_bien_ban_gui)) b 
+		ON b.Ma_bien_ban_nhan = bien_ban_nhan.Ma_bien_ban)
+	INNER JOIN khach_hang ON khach_hang.Ma_kh = bien_ban_nhan.ma_kh_nhan)
+	HAVING b.Ma_KH = Ma_KH_n AND b.Ngay_gui = Ngay_gui_n;
 END //
 
 CREATE PROCEDURE getCountNumber(
 	Ma_NV_n INT
 )
 BEGIN
-	select nhan_vien.Ma_NV, nhan_vien.Ten as Ten_tai_xe_lien_tinh, a.so_lan_tham_gia
-	from ((tai_xe_lien_tinh
-	inner join nhan_vien on nhan_vien.Ma_NV = tai_xe_lien_tinh.Ma_NV)
-	inner join 
-		(select ma_nv_tai_xe_lien_tinh, count(ma_nv_tai_xe_lien_tinh) as so_lan_tham_gia
-		from chuyen_xe_lien_tinh
-		group by ma_nv_tai_xe_lien_tinh) a on a.ma_nv_tai_xe_lien_tinh = tai_xe_lien_tinh.Ma_NV)
-	having nhan_vien.Ma_NV = Ma_NV_n;
+	SELECT nhan_vien.Ma_NV, nhan_vien.Ten AS Ten_tai_xe_lien_tinh, a.so_lan_tham_gia
+	FROM ((tai_xe_lien_tinh
+	INNER JOIN nhan_vien ON nhan_vien.Ma_NV = tai_xe_lien_tinh.Ma_NV)
+	INNER JOIN 
+		(SELECT ma_nv_tai_xe_lien_tinh, count(ma_nv_tai_xe_lien_tinh) AS so_lan_tham_gia
+		FROM chuyen_xe_lien_tinh
+		GROUP BY ma_nv_tai_xe_lien_tinh) a ON a.ma_nv_tai_xe_lien_tinh = tai_xe_lien_tinh.Ma_NV)
+	HAVING nhan_vien.Ma_NV = Ma_NV_n;
 END //
