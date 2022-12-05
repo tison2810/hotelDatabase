9eff4ec3cfcc064285d6aae8163ae824b2c6b847
Huy Dang <huy.dang110902@gmail.com>
2022-12-05 10:13:28 +0700

    Hurayyyyyyyyyy

--- frontend/manage_app/src/App.js
+++ frontend/manage_app/src/App.js
@@ -1,25 +1,37 @@
-import './App.css';
 import Home from './components/Home/Home';
 import Employee from './components/Function/Employee';
 import Customer from './components/Function/Customer';
 import Product from './components/Function/Product';
 import WareHouse from './components/Function/Warehouse';
 import Report from './components/Function/Report';
+import {Routes,Route, Link} from 'react-router-dom'
 
 
 function App() {
   return (
-    <>
-      <Home/>
-      <div style={{display:'flex', justifyContent:'space-around', width:'100%', marginTop:'100px', gap:'10px', marginLeft:'10px', marginRight:'10px'}}>
-        <Employee />
-        <Customer />
-        <Product />
-        <WareHouse />
-        <Report />
-      </div>
-    </>
+    <div>
+      <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+              </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<Employee/>} />
+      </Routes>
+      
+        </div>
+    
   );
 }
+
 
 export default App;
