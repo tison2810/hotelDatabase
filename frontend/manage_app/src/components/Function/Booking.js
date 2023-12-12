c5c645faa726a07bd1f1ff0d19982f491a44439c
NhatHa <ha.ngnhnht@gmail.com>
2023-12-13 00:18:14 +0700

    ok add dc cai booking

--- frontend/manage_app/src/components/Function/Booking.js
+++ frontend/manage_app/src/components/Function/Booking.js
@@ -1,99 +1,9 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import styles from "../../css/Part.module.css"
-import InsertForm from "./EmployeeFunction/InsertEmployee";
-import DeleteForm from "./EmployeeFunction/DeleteEmployee";
-import FilterForm from "./EmployeeFunction/FilterEmployee";
-import UpdateForm from "./EmployeeFunction/UpdateEmployee";
+import styles from "../../css/Part.module.css";
 
 
 const Booking = () => {
-    const [listEmployee, SetListEmployee] = useState([])
-    const [insertEmp, SetInsertEmp] = useState(false);
-    const [deleteEmp, SetDeleteEmp] = useState(false);
-    const [updateEmp, SetUpdateEmp] = useState(false);
-    const [filter, SetFilter] = useState(false);
-
-
-    function InsertEmployee() {
-        if (insertEmp === false) {
-            SetInsertEmp(true)
-            SetDeleteEmp(false)
-            SetFilter(false)
-            SetUpdateEmp(false)
-        }
-        else {
-            SetInsertEmp(false)
-            SetDeleteEmp(false)
-            SetFilter(false)
-            SetUpdateEmp(false)
-        }
-    }
-
-    function DeleteEmployee() {
-        if (deleteEmp === false) {
-            SetDeleteEmp(true)
-            SetFilter(false)
-            SetInsertEmp(false)
-            SetUpdateEmp(false)
-        }
-        else {
-            SetInsertEmp(false)
-            SetDeleteEmp(false)
-            SetFilter(false)
-            SetUpdateEmp(false)
-        }
-    }
-
-    function FilterEmployee() {
-        if (filter === false) {
-            SetFilter(true)
-            SetInsertEmp(false)
-            SetDeleteEmp(false)
-            SetUpdateEmp(false)
-        }
-        else {
-            SetInsertEmp(false)
-            SetDeleteEmp(false)
-            SetFilter(false)
-            SetUpdateEmp(false)
-        }
-    }
-
-    function UpdateEmployee() {
-        if (updateEmp === false) {
-            SetFilter(false)
-            SetInsertEmp(false)
-            SetDeleteEmp(false)
-            SetUpdateEmp(true)
-        }
-        else {
-            SetInsertEmp(false)
-            SetDeleteEmp(false)
-            SetFilter(false)
-            SetUpdateEmp(false)
-        }
-    }
-
-    useEffect(() => {
-        fetch("http://localhost:8080/employee/get")
-            .then(res => {
-                if (res.status !== 200) {
-                    throw new Error('Failed to fetch user status.');
-                }
-                return res.json();
-            })
-            .then(resData => {
-                SetListEmployee(resData);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-
-    }, [listEmployee])
 
     return (
         <div className={styles.container}>
@@ -102,80 +12,61 @@ const Booking = () => {
                 <p><i>Last updated 24h ago</i></p>
             </div>
             <main>
-            <Row xs={1} md={3} lg={4} className="g-4">
-                <Col>
-                    <Card>
-                        <Card.Img variant="top" src="" />
-                        <Card.Body>
-                            <Card.Title>STAND01</Card.Title>
-                            <Card.Text>
-                                <p>Mô tả: Phòng tiêu chuẩn 2 người</p>
-                                <p>Số người tối đa: 2</p>
-                                <p>Mức giá: 200000</p>
-                            </Card.Text>
+                <div className={styles.flex_container}>
+                    <div className={styles.custom_card}>
+                        <div>
+                            <img src="https://picsum.photos/200/300" alt="img" />
+                        </div>
+                        <div>
+                            <h2>STAND01</h2>
+                            <p>Mô tả: Phòng tiêu chuẩn 2 người</p>
+                            <p>Số người tối đa: 2</p>
+                            <p>Mức giá: 200000</p>
+                            <br></br>
                             <button  className={styles.button}>Book</button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card>
-                        <Card.Img variant="top" src="" />
-                        <Card.Body>
-                            <Card.Title>STAND02</Card.Title>
-                            <Card.Text>
-                                <p>Mô tả: Phòng tiêu chuẩn 4 người</p>
-                                <p>Số người tối đa: 4</p>
-                                <p>Mức giá: 300000</p>
-                            </Card.Text>
+                        </div>
+                    </div>
+                    <div className={styles.custom_card}>
+                        <div>
+                            <img src="https://picsum.photos/200/300" alt="img" />
+                        </div>
+                        <div>
+                            <h2>STAND02</h2>
+                            <p>Mô tả: Phòng tiêu chuẩn 4 người</p>
+                            <p>Số người tối đa: 4</p>
+                            <p>Mức giá: 300000</p>
+                            <br></br>
                             <button  className={styles.button}>Book</button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card>
-                        <Card.Img variant="top" src="" />
-                        <Card.Body>
-                            <Card.Title>VIP01</Card.Title>
-                            <Card.Text>
-                                <p>Mô tả: Phòng VIP tiêu chuẩn</p>
-                                <p>Số người tối đa: 2</p>
-                                <p>Mức giá: 600000</p>
-                            </Card.Text>
+                        </div>
+                    </div>
+                    <div className={styles.custom_card}>
+                        <div>
+                            <img src="https://picsum.photos/200/300" alt="img" />
+                        </div>
+                        <div>
+                            <h2>VIP01</h2>
+                            <p>Mô tả: Phòng VIP tiêu chuẩn</p>
+                            <p>Số người tối đa: 2</p>
+                            <p>Mức giá: 600000</p>
+                            <br></br>
                             <button  className={styles.button}>Book</button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card>
-                        <Card.Img variant="top" src="" />
-                        <Card.Body>
-                            <Card.Title>VIP02</Card.Title>
-                            <Card.Text>
-                                <p>Mô tả: Phòng VIP cao cấp</p>
-                                <p>Số người tối đa: 2</p>
-                                <p>Mức giá: 1000000</p>
-                            </Card.Text>
+                        </div>
+                    </div>
+                    <div className={styles.custom_card}>
+                        <div>
+                            <img src="https://picsum.photos/200/300" alt="img" />
+                        </div>
+                        <div>
+                            <h2>VIP02</h2>
+                            <p>Mô tả: Phòng VIP cao cấp</p>
+                            <p>Số người tối đa: 2</p>
+                            <p>Mức giá: 1000000</p>
+                            <br></br>
                             <button  className={styles.button}>Book</button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
+                        </div>
+                    </div>
+                </div>
             </main>
-            <div className={styles.button_container}>
-                <button onClick={InsertEmployee} className={styles.button}>Add Employee</button>
-                <button onClick={DeleteEmployee} className={styles.button}>Delete Employee</button>
-                <button onClick={FilterEmployee} className={styles.button}>Search Employee</button>
-                <button onClick={UpdateEmployee} className={styles.button}>Update Employee</button>
-            </div>
-            
-            {(insertEmp && !deleteEmp && !filter && !updateEmp) && <InsertForm/>
-            } 
-            {(!insertEmp && deleteEmp && !filter && !updateEmp) && <DeleteForm/>
-            } 
-            {(!insertEmp && !deleteEmp && filter && !updateEmp) && <FilterForm/>
-            } 
-            {(!insertEmp && !deleteEmp && !filter && updateEmp) && <UpdateForm/>
-            }
         </div>
     )
 }
