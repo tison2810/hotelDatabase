027a7c845a1a10f3c41061f8ed7be3417169a772
tison2810 <son.nguyenthai@hcmut.edu.vn>
2023-12-12 23:18:35 +0700

    huhu

--- frontend/manage_app/src/App.js
+++ frontend/manage_app/src/App.js
@@ -1,9 +1,10 @@
 import Home from './components/Home/Home';
-
+import Booking from './components/Function/Booking';
 function App() {
   return (
     <div>
-      <Home/>
+      {/* <Home/> */}
+      <Booking/>
     </div>
   );
 }
--- frontend/manage_app/src/components/Function/Booking.js
+++ frontend/manage_app/src/components/Function/Booking.js
@@ -4,97 +4,10 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import styles from "../../css/Part.module.css"
-import InsertForm from "./EmployeeFunction/InsertEmployee";
-import DeleteForm from "./EmployeeFunction/DeleteEmployee";
-import FilterForm from "./EmployeeFunction/FilterEmployee";
-import UpdateForm from "./EmployeeFunction/UpdateEmployee";
+import ReactDOM from "react-dom";
 
 
 const Booking = () => {
-    const [listEmployee, SetListEmployee] = useState([])
-    const [insertEmp, SetInsertEmp] = useState(false);
-    const [deleteEmp, SetDeleteEmp] = useState(false);
-    const [updateEmp, SetUpdateEmp] = useState(false);
-    const [filter, SetFilter] = useState(false);
-
-
-    function InsertEmployee() {
-        if (insertEmp === false) {
-            SetInsertEmp(true)
-            SetDeleteEmp(false)
-            SetFilter(false)
-            SetUpdateEmp(false)
-        }
-        else {
-            SetInsertEmp(false)
-            SetDeleteEmp(false)
-            SetFilter(false)
-            SetUpdateEmp(false)
-        }
-    }
-
-    function DeleteEmployee() {
-        if (deleteEmp === false) {
-            SetDeleteEmp(true)
-            SetFilter(false)
-            SetInsertEmp(false)
-            SetUpdateEmp(false)
-        }
-        else {
-            SetInsertEmp(false)
-            SetDeleteEmp(false)
-            SetFilter(false)
-            SetUpdateEmp(false)
-        }
-    }
-
-    function FilterEmployee() {
-        if (filter === false) {
-            SetFilter(true)
-            SetInsertEmp(false)
-            SetDeleteEmp(false)
-            SetUpdateEmp(false)
-        }
-        else {
-            SetInsertEmp(false)
-            SetDeleteEmp(false)
-            SetFilter(false)
-            SetUpdateEmp(false)
-        }
-    }
-
-    function UpdateEmployee() {
-        if (updateEmp === false) {
-            SetFilter(false)
-            SetInsertEmp(false)
-            SetDeleteEmp(false)
-            SetUpdateEmp(true)
-        }
-        else {
-            SetInsertEmp(false)
-            SetDeleteEmp(false)
-            SetFilter(false)
-            SetUpdateEmp(false)
-        }
-    }
-
-    useEffect(() => {
-        fetch("http://localhost:8080/employee/get")
-            .then(res => {
-                if (res.status !== 200) {
-                    throw new Error('Failed to fetch user status.');
-                }
-                return res.json();
-            })
-            .then(resData => {
-                SetListEmployee(resData);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-
-    }, [listEmployee])
-
     return (
         <div className={styles.container}>
             <h1>Booking</h1>
@@ -161,21 +74,6 @@ const Booking = () => {
                 </Col>
             </Row>
             </main>
-            <div className={styles.button_container}>
-                <button onClick={InsertEmployee} className={styles.button}>Add Employee</button>
-                <button onClick={DeleteEmployee} className={styles.button}>Delete Employee</button>
-                <button onClick={FilterEmployee} className={styles.button}>Search Employee</button>
-                <button onClick={UpdateEmployee} className={styles.button}>Update Employee</button>
-            </div>
-            
-            {(insertEmp && !deleteEmp && !filter && !updateEmp) && <InsertForm/>
-            } 
-            {(!insertEmp && deleteEmp && !filter && !updateEmp) && <DeleteForm/>
-            } 
-            {(!insertEmp && !deleteEmp && filter && !updateEmp) && <FilterForm/>
-            } 
-            {(!insertEmp && !deleteEmp && !filter && updateEmp) && <UpdateForm/>
-            }
         </div>
     )
 }
